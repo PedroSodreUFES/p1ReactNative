d0b74cd207d184f7ef536745df894994d0dd45e6
PedroSodreUFES <pedro.malini@edu.ufes.br>
2024-12-20 14:26:21 -0300

    v0

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native';
+
+export default function App(){ {/*COmponentes tem de ter nome maiusculo*/}
+  return(
+    <View>{/* mesma coisa que <></> */}
+      <Text>React Native la em cima</Text>
+      <Text>React Native mais embaixo</Text>
+    </View>
+  )
+}
